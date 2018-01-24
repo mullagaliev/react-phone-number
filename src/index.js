163ca64ab3d3506bb30fe43c8d160959ff7dc9c0
Andrew <vray1995@gmail.com>
2018-01-24 12:47:11 +0300

    add formatted number by default
    1. Add varibale default_formatted_number (equals param 'number')
    2. Update gitignore

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const React = require('react');
 
 const PhoneNumber = ({ number, isLinked, className }) => {
     if (!number) return null;
-    
+
+    const default_formatted_number = number;
     number = number.toString().replace(/[^0-9\+]+/g, '').replace(/\s+/, '');
     
     let formatted_number;
@@ -14,8 +15,10 @@ const PhoneNumber = ({ number, isLinked, className }) => {
         formatted_number = number.replace(/([0-9]{4})([0-9]{3})([0-9]{3})/, "$1 $2 $3");
     } else if (number.indexOf('+') == 0) {
         formatted_number = number.replace(/([0-9]{2})([0-9]{3})([0-9]{3})([0-9]{3})/, "$1 $2 $3 $4");
+    } else {
+        formatted_number = default_formatted_number;
     }
-    
+
     if (isLinked) {
         return (<a href={`tel:${number}`} className={className}>{formatted_number}</a>);
     } else {
